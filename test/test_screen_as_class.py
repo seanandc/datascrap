8772bb00d722f4255a52aef081650abb5a84f198
junkmob <gregcorradini@gmail.com>
2015-08-23 09:06:46 -0700

    make tests better and more robust

--- test/test_screen_as_class.py
+++ test/test_screen_as_class.py
@@ -3,7 +3,7 @@ from datascrap.screens import StockScreen
 import os
 
 
-class TestScreenAsFunction(unittest.TestCase):
+class TestScreenParsingAndOutput(unittest.TestCase):
 
     def setUp(self):
         self.sp500_test_set = ['a', 'aa',
@@ -14,8 +14,108 @@ class TestScreenAsFunction(unittest.TestCase):
                                'adi', 'adm',
                                'adp']
 
-        self.yahoo_finance_html_path = os.path.join(os.path.split(os.path.abspath(__file__))[0],"yahoo_finance_output_082215_10:00:00.html")
-        self.yahoo_finance_html = open(self.yahoo_finance_html_path,"r").read()
+        self.yahoo_finance_html_path = os.path.join(
+            os.path.split(os.path.abspath(__file__))[0],
+            "yahoo_finance_%s_09:00-00:00:00.html"
+        )
+
+
+    def test_request_parse_filter_by_10(self):
+        '''
+        verify that a certain number filters
+        and stock returns the results from out static html
+        :return:
+        '''
+
+        # ARRANGE
+        test_stock = 'aci'
+        screen = StockScreen(10)
+        yahoo_finance_html = open(self.yahoo_finance_html_path%test_stock,"r").read()
+        expected_screen_output = "price to book ratio: aci, 0.05\nPEG ration: aci -0.01\n"
+
+        # ACT
+        screen_output = screen._parse(test_stock,yahoo_finance_html)
+        print screen_output
+
+        # ASSERT
+        self.assertEqual(expected_screen_output,screen_output)
+
+    def test_screen_parse_filter_by_1(self):
+        '''
+        verify that a certain number filters
+        and stock returns the results from out static html
+        :return:
+        '''
+
+        # ARRANGE
+        test_stock = 'aci'
+        screen = StockScreen(1)
+        yahoo_finance_html = open(self.yahoo_finance_html_path%test_stock,"r").read()
+        expected_screen_output = "price to book ratio: aci, 0.05\nPEG ration: aci -0.01\n"
+
+        # ACT
+        screen_output = screen._parse(test_stock,yahoo_finance_html)
+        print screen_output
+
+        # ASSERT
+        self.assertEqual(expected_screen_output,screen_output)
+
+    def test_screen_parse_filter_by_5(self):
+        '''
+        verify that a certain number filters
+        and stock returns the results from out static html
+        :return:
+        '''
+
+        # ARRANGE
+        test_stock = 'aci'
+        screen = StockScreen(5)
+        yahoo_finance_html = open(self.yahoo_finance_html_path%test_stock,"r").read()
+        expected_screen_output = "price to book ratio: aci, 0.05\nPEG ration: aci -0.01\n"
+
+        # ACT
+        screen_output = screen._parse(test_stock,yahoo_finance_html)
+        print screen_output
+
+        # ASSERT
+        self.assertEqual(expected_screen_output,screen_output)
+
+    def test_screen_parse_underfilter_by_3_(self):
+        '''
+        verify that a certain number does not filter
+        correctly when our target is over the filter
+        '''
+
+        # ARRANGE
+        test_stock = 'a'
+        screen = StockScreen(3.0)
+        yahoo_finance_html = open(self.yahoo_finance_html_path%test_stock,"r").read()
+
+        # ACT
+        screen_output = screen._parse(test_stock,yahoo_finance_html)
+
+        # ASSERT
+        self.assertEqual(screen_output, "")
+
+    def test_screen_parse_filter_by_100_all_test_sps(self):
+        '''
+        pick a number filter that is really big
+        and make sure that all our test indexes
+        return a value from their respective
+        html files that is not an empty string
+        :return:
+        '''
+
+        # ARRANGE
+        screen = StockScreen(100)
+        for test_stock in self.sp500_test_set:
+            yahoo_finance_html = open(self.yahoo_finance_html_path%test_stock,"r").read()
+
+            # ACT
+            screen_output = screen._parse(test_stock,yahoo_finance_html)
+
+            # ASSERT
+            self.assertTrue(screen_output)
 
 
     @unittest.skip("TODO: need to test for failure with mock")
@@ -25,31 +125,3 @@ class TestScreenAsFunction(unittest.TestCase):
     @unittest.skip("TODO: need to test for index errors using mock")
     def test_screen_index_errors(self):
         pass
-
-    def test_request_parse_filter_by_10(self):
-        '''
-        verify that a certain number filter
-        and stock returns the results from out static html
-        :return:
-        '''
-
-        # ARRANGE
-        screen = StockScreen(10)
-        expected_screen_output = "price to book ratio: aci, 8.02\nPEG ration: aci 2.43\n"
-
-        # ACT
-        screen_output = screen._parse('aci',self.yahoo_finance_html)
-
-        # ASSERT
-        self.assertEqual(expected_screen_output,screen_output)
-
-
-
-    @unittest.skip("TODO: need more tests around different number filters")
-    def test_screen_parse_filter_by_1(self):
-        pass
-
-    @unittest.skip("TODO: need more tests around different number filters")
-    def test_screen_parse_filter_by_5(self):
-        pass
-
